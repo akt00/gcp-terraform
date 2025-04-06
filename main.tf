f5cd2b36e0cb6c33bdc1f6c54e739139a951ba6e
Aki <msf5pbsi@gmail.com>
2025-04-06 19:26:44 +0900

    temp

--- main.tf
+++ main.tf
@@ -17,6 +17,27 @@ resource "google_compute_subnetwork" "network-with-private-secondary-ip-ranges" 
   network       = google_compute_network.vpc_network.id
 }
 
+resource "google_compute_firewall" "default" {
+  name    = "allow-internal"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["0-65535"]
+  }
+
+  allow {
+    protocol = "udp"
+    ports = ["0-65535"]
+  }
+
+  source_ranges = ["10.1.0.0/16"]
+}
+
 module "instance_template" {
   source  = "terraform-google-modules/vm/google//modules/instance_template"
   automatic_restart = true
